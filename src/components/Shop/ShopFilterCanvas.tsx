e3ea8652e44585ced18459133c6e3369785f3ea4
alexdev-commits <alexdev.end@gmail.com>
2025-10-21 19:37:13 +0100

    ?

--- src/components/Shop/ShopFilterCanvas.tsx
+++ src/components/Shop/ShopFilterCanvas.tsx
@@ -13,7 +13,7 @@ interface Props {
     data: Array<ProductType>;
     productPerPage: number
     dataType: string | null
-    productStyle?: string; // optional now
+    productStyle?: string
 }
 
 const ShopFilterCanvas: React.FC<Props> = ({ data, productPerPage, dataType, productStyle }) => {
