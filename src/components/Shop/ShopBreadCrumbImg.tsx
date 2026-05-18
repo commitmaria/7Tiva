01979fb3dddd70798a77a3de0ab6386bda7c4589
commitmaria <marias24u@hotmail.com>
2026-05-18 12:45:54 +0200

    update

--- src/components/Shop/ShopBreadCrumbImg.tsx
+++ src/components/Shop/ShopBreadCrumbImg.tsx
@@ -482,30 +482,7 @@ const ShopBreadCrumbImg: React.FC<Props> = ({
                 </div>
               </div>
               <div>
-                <div className="filter-size">
-                  <div className="heading6">Size</div>
-                  <div className="list-size flex items-center flex-wrap gap-3 gap-y-4 mt-4">
-                    {["XS", "S", "M", "L", "XL", "2XL", "3XL"].map(
-                      (item, index) => (
-                        <div
-                          key={index}
-                          className={`size-item text-button w-[44px] h-[44px] flex items-center justify-center rounded-full border border-line ${size === item ? "active" : ""
-                            }`}
-                          onClick={() => handleSize(item)}
-                        >
-                          {item}
-                        </div>
-                      )
-                    )}
-                    <div
-                      className={`size-item text-button px-4 py-2 flex items-center justify-center rounded-full border border-line ${size === "freesize" ? "active" : ""
-                        }`}
-                      onClick={() => handleSize("freesize")}
-                    >
-                      Freesize
-                    </div>
-                  </div>
-                </div>
+                
                 <div className="filter-price mt-8">
                   <div className="heading6">Price Range</div>
                   <Slider
@@ -532,71 +509,11 @@ const ShopBreadCrumbImg: React.FC<Props> = ({
                   </div>
                 </div>
               </div>
-              <div className="filter-color">
-                <div className="heading6">colors</div>
-                <div className="list-color flex items-center flex-wrap gap-3 gap-y-4 mt-4">
-                  <div
-                    className={`color-item px-3 py-[5px] flex items-center justify-center gap-2 rounded-full border border-line ${color === "pink" ? "active" : ""
-                      }`}
-                    onClick={() => handleColor("pink")}
-                  >
-                    <div className="color bg-[#F4C5BF] w-5 h-5 rounded-full"></div>
-                    <div className="caption1 capitalize">pink</div>
-                  </div>
-                  <div
-                    className={`color-item px-3 py-[5px] flex items-center justify-center gap-2 rounded-full border border-line ${color === "red" ? "active" : ""
-                      }`}
-                    onClick={() => handleColor("red")}
-                  >
-                    <div className="color bg-red w-5 h-5 rounded-full"></div>
-                    <div className="caption1 capitalize">red</div>
-                  </div>
-                  <div
-                    className={`color-item px-3 py-[5px] flex items-center justify-center gap-2 rounded-full border border-line ${color === "green" ? "active" : ""
-                      }`}
-                    onClick={() => handleColor("green")}
-                  >
-                    <div className="color bg-green w-5 h-5 rounded-full"></div>
-                    <div className="caption1 capitalize">green</div>
-                  </div>
-                  <div
-                    className={`color-item px-3 py-[5px] flex items-center justify-center gap-2 rounded-full border border-line ${color === "yellow" ? "active" : ""
-                      }`}
-                    onClick={() => handleColor("yellow")}
-                  >
-                    <div className="color bg-yellow w-5 h-5 rounded-full"></div>
-                    <div className="caption1 capitalize">yellow</div>
-                  </div>
-                  <div
-                    className={`color-item px-3 py-[5px] flex items-center justify-center gap-2 rounded-full border border-line ${color === "purple" ? "active" : ""
-                      }`}
-                    onClick={() => handleColor("purple")}
-                  >
-                    <div className="color bg-purple w-5 h-5 rounded-full"></div>
-                    <div className="caption1 capitalize">purple</div>
-                  </div>
-                  <div
-                    className={`color-item px-3 py-[5px] flex items-center justify-center gap-2 rounded-full border border-line ${color === "black" ? "active" : ""
-                      }`}
-                    onClick={() => handleColor("black")}
-                  >
-                    <div className="color bg-black w-5 h-5 rounded-full"></div>
-                    <div className="caption1 capitalize">black</div>
-                  </div>
-                  <div
-                    className={`color-item px-3 py-[5px] flex items-center justify-center gap-2 rounded-full border border-line ${color === "white" ? "active" : ""
-                      }`}
-                    onClick={() => handleColor("white")}
-                  >
-                    <div className="color bg-[#F6EFDD] w-5 h-5 rounded-full"></div>
-                    <div className="caption1 capitalize">white</div>
-                  </div>
-                </div>
-              </div>
+              
               <div className="filter-brand">
                 <div className="heading6">Brands</div>
                 <div className="list-brand mt-4">
-                  {["adidas", "hermes", "zara", "nike", "gucci"].map(
+                  {["Followers", "Serial Code", "Activation", "Invitaion", "Service"].map(
                     (item, index) => (
                       <div
                         key={index}
